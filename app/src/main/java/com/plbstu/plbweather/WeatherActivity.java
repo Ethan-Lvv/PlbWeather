9f597b3a63027a1f8e22480d38cb7a8ed783a7d5
Stacy-Lv <1902418946@qq.com>
2020-12-10 16:43:02 +0800

    天气显示界面优化

--- app/src/main/java/com/plbstu/plbweather/WeatherActivity.java
+++ app/src/main/java/com/plbstu/plbweather/WeatherActivity.java
@@ -2,6 +2,8 @@ package com.plbstu.plbweather;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -53,6 +55,13 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        // 使状态栏设置为透明色使界面更加美观一体
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
 
         // 初始化各控件
         bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
